a869b925e5c0005fc00ea08779871890f44f44ae
HRP117 <72773916+HRP117@users.noreply.github.com>
2025-05-02 19:43:08 -0600

    Prueba de control de puntos de referencia para detecion de rostros

---
+++ test_mediapipe.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+
+        cv2.imshow('MediaPipe Face Detection', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
